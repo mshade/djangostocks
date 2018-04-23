e03018aea0bb0a9701a491af5e5bde4558900e4b
Mike Shade <mshade@mshade.org>
2018-04-22 23:57:41 -0400

    define URL of JSON to parse via settings / .env

--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.conf import settings
 import requests
 import json
 
@@ -10,24 +11,34 @@ import json
 #     return render(request, 'quotes/home.html', context)
 
 def home(request):
-    response = requests.get('http://localhost:8000/static/quotes_data.json')
-    quotes_list = response.json()
+    try:
+        response = requests.get(settings.QUOTES_URL)
+        quotes_list = response.json()
 
-    return render(request, 'quotes/home.html', {'context': quotes_list})
-
+        return render(request, 'quotes/home.html', {'context': quotes_list})
+    except:
+        return HttpResponse("""
+        Problem retreiving JSON. Is QUOTES_URL correct?
+        """)
 
 def symbol(request, stock_symbol):
-    response = requests.get('http://localhost:8000/static/quotes_data.json')
-    quotes_list = response.json()
-    company_data = {}
+    try:
+        response = requests.get(settings.QUOTES_URL)
+        quotes_list = response.json()
+        company_data = {}
 
-    for company in quotes_list:
-        if stock_symbol in company['Symbol']:
-            company_data = company
-            break
+        for company in quotes_list:
+            if stock_symbol in company['Symbol']:
+                company_data = company
+                # Quit looking if we hit a match
+                break
 
-    if company_data:
-        return render(request, 'quotes/symbol.html', {'context': company_data})
-    else:
-        return render(request, 'quotes/404.html', status="404")
+        if company_data:
+            return render(request, 'quotes/symbol.html', {'context': company_data})
+        else:
+            return render(request, 'quotes/404.html', status="404")
+    except:
+        return HttpResponse("""
+        Problem retreiving JSON. Is QUOTES_URL correct?
+        """)
 
--- stocks/settings.py
+++ stocks/settings.py
@@ -146,3 +146,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Quotes Data JSON URL
+QUOTES_URL = env('QUOTES_URL')
